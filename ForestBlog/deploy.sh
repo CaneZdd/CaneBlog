57940d44b37f44bd8e3f1861760d8d276ec29779
CaneZdd <1114067114@qq.com>
2018-10-01 15:10:57 +0800

    deploy.sh to Forest

--- ForestBlog/deploy.sh
+++ ForestBlog/deploy.sh
@@ -20,24 +20,21 @@ killTomcat()
       kill -9 $pid
     fi
 }
-cd $PROJ_PATH/order
+cd $PROJ_PATH/CaneBlog/ForestBlog
 mvn clean install
 
 # 停tomcat
 killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -rf $TOMCAT_APP_PATH/webapps/ForestBlog
+rm -f $TOMCAT_APP_PATH/webapps/ForestBlog.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+cp $PROJ_PATH/CaneBlog/ForestBlog/target/ForestBlog.war $TOMCAT_APP_PATH/webapps/
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
+pwd
 sh bin/startup.sh
 
